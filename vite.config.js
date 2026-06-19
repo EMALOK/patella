9f38911d9187d43ac3e7d33ddb20448bd4308c79
EMALOK <emanuele.riccilucchi@gmail.com>
2026-06-19 19:48:20 +0200

    git pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1];
 export default defineConfig({
+    base: repositoryName ? `/${repositoryName}/` : '/',
     plugins: [react()],
 });
